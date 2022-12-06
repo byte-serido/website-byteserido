ca003243715bd24617dca319f69d6ef554570bd9
JFmaia <jfmaia741@gmail.com>
2022-12-06 16:44:13 -0300

    fix: Ajustes

--- byteserido/src/views/HomeView.vue
+++ byteserido/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
   </main>
 </template>
