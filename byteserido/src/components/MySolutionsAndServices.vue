335b4fda6e62b940af70f1cb8c994d71b8f0c74b
JFmaia <jfmaia741@gmail.com>
2022-12-16 14:06:06 -0300

    feat: Criando novo componente #6

---
+++ byteserido/src/components/MySolutionsAndServices.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="sands-container">
+        <div class="container sands">
+            <p>SERVIÇOS E SOLUÇÕES INOVADORAS</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            toggleServices: false,
+            toggleSolutions: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .sands-container{
+        background-color: #111010;
+    }
+
+    .sands-container .sands{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 30px 128px 126px 128px;
+        gap: 77px;
+    }
+
+    .sands p {
+        font-family: "Exo", sans-serif;
+        font-weight: 700;
+        font-size: 2.5 rem;
+        color: #FFFFFF;
+    }
+</style>
