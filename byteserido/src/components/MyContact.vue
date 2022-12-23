d69bd6cf4e9cf9da8eeb03866ec868ba736922f6
JFmaia <jfmaia741@gmail.com>
2022-12-23 14:18:44 -0300

    feat: Adicionando urls nos icons #13

--- byteserido/src/components/MyContact.vue
+++ byteserido/src/components/MyContact.vue
@@ -6,31 +6,31 @@
             <hr>
             <p class="options-socias">Siga nossas redes sociais:</p>
             <div class="list-social">
-                <a class="item" rel="stylesheet" href="" target="_blank">
+                <a class="item" rel="stylesheet" href="https://www.facebook.com/profile.php?id=100072218402852" target="_blank">
                     <img src="../assets/social/icon_facebook.svg" alt="Icone do Facebook">
                 </a>
 
-                <a class="item" rel="stylesheet" href="" target="_blank">
+                <a class="item" rel="stylesheet" href="https://www.instagram.com/byteseridojr/" target="_blank">
                     <img src="../assets/social/icon_instagram.svg" alt="Icone do Instagram">
                 </a>
 
-                <a class="item" rel="stylesheet" href="" target="_blank">
+                <a class="item" rel="stylesheet" href="https://twitter.com/ByteSerido" target="_blank" >
                     <img src="../assets/social/icon_twitter.svg" alt="Icone do Twitter">
                 </a>
 
-                <a class="item" rel="stylesheet" href="" target="_blank">
+                <a class="item" rel="stylesheet" href="https://www.linkedin.com/company/byte-serid%C3%B3-jr/" target="_blank">
                     <img src="../assets/social/icon_linkdin.svg" alt="Icone do Linkedin">
                 </a>
 
-                <a class="item" rel="stylesheet" href="" target="_blank">
+                <a class="item" rel="stylesheet" href="https://github.com/byte-serido" target="_blank">
                     <img src="../assets/social/icon_github.svg" alt="Icone do GitHub">
                 </a>
 
-                <a class="item" rel="stylesheet" href="" target="_blank">
+                <a class="item" title="(83)9-9801-0847" rel="stylesheet" href="https://wa.me/558398010847" target="_blank">
                     <img src="../assets/social/icon_whatsaap.svg" alt="Icone do Whatsaap">
                 </a>
 
-                <a class="item" rel="stylesheet" href="" target="_blank">
+                <a class="item" rel="stylesheet" title="byteseridoceres@gmail.com" target="_blank" href="mailto:byteseridoceres@gmail.com?subject=Hello%20again">
                     <img src="../assets/social/icon_email.svg" alt="Icone do E-mail">
                 </a>
 
